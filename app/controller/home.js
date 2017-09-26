5e0567ca0090ba816fb8a0946b48c91c3dd0e08f
820514 <820514@nd>
2017-09-26 20:00:38 +0800

    ESLint 规则改为 eslint-config-standard

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,9 @@
-'use strict';
-
 module.exports = app => {
   class HomeController extends app.Controller {
-    * index() {
-      this.ctx.body = 'hi, egg';
+    * index () {
+      this.ctx.body = 'hi, egg'
     }
   }
-  return HomeController;
-};
+
+  return HomeController
+}
